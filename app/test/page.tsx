4202f87299f114d44f13d39b6e5e4c77bae09db8
baram55 <asd7973@gmail.com>
2024-04-03 03:57:59 +0900

    [Test] : oAuth 로그인에 성공하면 닉네임 입력을 받음

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React, { useEffect } from "react";
-import { getUserInfo, getUserNickname, oAuthKakaoLogIn, updateNickname } from "../_utils/supabase/authAPI";
+import { getUserInfo, getUserNickname, oAuthKakaoLogIn, updateUserNickname } from "../_utils/supabase/authAPI";
 
 const Test = () => {
   useEffect(() => {
     const test = async () => {
       const nickname = window.prompt("닉네임을 입력하세요.", "닉네임");
       if (nickname) {
-        await updateNickname(nickname);
+        await updateUserNickname(nickname);
+        console.log(await getUserNickname());
       }
     };
 
