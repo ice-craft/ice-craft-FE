5bda6b7fdf60fe67c660cbb8b5fa6a3b9d0dcb6c
이준구 <leejungoo1396@gmail.com>
2024-04-02 16:04:10 +0900

    [Script #8]  livekit 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` 
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'FiveDotSix',
+  title: 'iceCraft',
   description: '화상 채팅 기반으로한 게임 서비스'
 };
 
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,11 +1,25 @@
-import Link from 'next/link';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export const MainPage = () => {
-  const id = 1;
+  // TODO: get user input for room and name
+  const [room, setRoom] = useState<string>('');
+  const [name, setName] = useState<string>('');
+
+  const router = useRouter();
+
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`/room/${room}?room=${room}&name=${name}`);
+  };
 
   return (
-    <>
-      <Link href={`/room/${id}`}>마피아 방으로 이동합니다</Link>
-    </>
+    <form onSubmit={onSubmitHandler} className="flex flex-col justify-center item-center min-h-screen">
+      <input type="text" placeholder="Room" value={room} className="mb-4" onChange={(e) => setRoom(e.target.value)} />
+      <input type="text" placeholder="Name" value={name} className="mb-4" onChange={(e) => setName(e.target.value)} />
+      <button type="submit">방 생성</button>
+    </form>
   );
 };
