ec6b1d32328cdc601b9719f53fa06e508ca15163
이준구 <leejungoo1396@gmail.com>
2024-04-02 18:10:34 +0900

    Merge branch 'dev' of https://github.com/TeamSparta-Project/ice-craft into 8-feat

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const MainPage = () => {
   // TODO: get user input for room and name
-  const [room, setRoom] = useState<string>('');
-  const [name, setName] = useState<string>('');
+  const [room, setRoom] = useState<string>("");
+  const [name, setName] = useState<string>("");
 
   const router = useRouter();
 
-  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+  const SubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     router.push(`/room/${room}?room=${room}&name=${name}`);
   };
 
   return (
-    <form onSubmit={onSubmitHandler} className="flex flex-col justify-center item-center min-h-screen">
+    <form onSubmit={SubmitHandler} className="flex flex-col justify-center item-center min-h-screen">
       <input type="text" placeholder="Room" value={room} className="mb-4" onChange={(e) => setRoom(e.target.value)} />
       <input type="text" placeholder="Name" value={name} className="mb-4" onChange={(e) => setName(e.target.value)} />
       <button type="submit">방 생성</button>
