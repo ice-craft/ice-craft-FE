29071afaa077d3cfd45506f29a7fb0df8af4cf84
baram55 <asd7973@gmail.com>
2024-04-03 19:40:00 +0900

    [Feat] : 새로운 유저가 oAuth를 통해서 로그인 했을 때 닉네임을 입력받는 로직 추가

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -8,13 +8,37 @@ import {
   logOut,
   updateUserNickname
 } from "../_utils/supabase/authAPI";
-import { checkUserRegistered, duplicateCheckUserNickname } from "../_utils/supabase/accountAPI";
+import { checkUserRegistered, duplicateCheckUserNickname, registerAccount } from "../_utils/supabase/accountAPI";
 
 const Test = () => {
   useEffect(() => {
     const test = async () => {
-      const data = await checkUserRegistered("test4@test.com");
-      console.log(data);
+      const userEmail = await getUserEmail();
+      const isUserRegistered = await checkUserRegistered(userEmail!);
+      if (isUserRegistered) {
+        return;
+      }
+      while (true) {
+        let inputNickname = window.prompt("닉네임을 입력하세요.");
+        if (inputNickname && 2 <= inputNickname.length && inputNickname.length <= 6) {
+          const isExistedNickname = await duplicateCheckUserNickname(inputNickname);
+          if (!isExistedNickname) {
+            const email = await getUserEmail();
+            const nickname = await getUserNickname();
+            const uid = await getUserUid();
+            if (email && nickname && uid) {
+              await registerAccount(uid, email, nickname);
+              break;
+            }
+          } else {
+            console.log("이미 존재하는 닉네임입니다."); //NOTE - 테스트 코드입니다.
+            continue;
+          }
+        } else {
+          console.log("닉네임의 조건이 맞지않습니다."); //NOTE - 테스트 코드입니다.
+          continue;
+        }
+      }
     };
     test();
   }, []);
