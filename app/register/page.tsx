9c8884ed0bbd62c2b79e4fd34737771d86431198
baram55 <asd7973@gmail.com>
2024-04-04 01:19:14 +0900

    [Feat] : 이메일 입력에 대한 유효성 검사 구현

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,28 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
+
+  const emailChangeHandler = (inputEmail: string) => {
+    setEmail(inputEmail);
+
+    if (inputEmail.length === 0) {
+      return setEmailErrorMessage("이메일을 입력해주세요.");
+    }
+
+    let emailPattern = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/);
+    const isEmail = emailPattern.test(inputEmail);
+
+    if (!isEmail) {
+      return setEmailErrorMessage("이메일 형식이 아닙니다.");
+    }
+    setEmailErrorMessage("");
+  };
   return (
     <form className="flex flex-col justify-center flex-1 w-2/3 gap-2 p-4 m-4">
       <h1 className="text-center">회원가입</h1>
@@ -12,10 +34,12 @@ const Register = () => {
           className="px-4 py-2 mb-6 border rounded-md bg-inherit"
           name="email"
           placeholder="이메일을 입력해주세요."
+          value={email}
+          onChange={(e) => emailChangeHandler(e.target.value)}
           required
         />
         <button className="bg-slate-300">중복확인</button>
-        <p className="text-red-500">이메일 에러</p>
+        <p className="text-red-500">{emailErrorMessage}</p>
       </div>
 
       <label className="text-md" htmlFor="nickname">
@@ -51,7 +75,7 @@ const Register = () => {
         required
       />
       <p className="text-red-500">비밀번호 확인 에러</p>
-      <button>회원가입</button>
+      <button className="bg-slate-300">회원가입</button>
       <div className="flex flex-col gap-2">
         <p className="text-center">간편 가입하기</p>
         <button className="bg-slate-300">카카오톡으로 회원가입</button>
