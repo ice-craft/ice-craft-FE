5b4d68096f90e739d0560d96d03bf7198c004c69
박서영 <seokachuu@gmail.com>
2024-04-06 01:08:04 +0900

    [Style #42] mainpage css layout 생성

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,9 +1,83 @@
 import React from "react";
 import RoomButton from "../_components/mainpageComponents/Roombutton";
+import Link from "next/link";
+import Image from "next/image";
+import MafiaVisual from "@/public/images/mafia_visual.png";
+import MafiaItem from "@/public/images/mafia_item.png";
+import S from "@/app/_style/mainPage/main.module.css";
 
 const Mainpage = () => {
   return (
-    <main>
+    <main className={S.main}>
+      {/* <section className={S.visualSection}>
+        <div className={S.visual}>
+          <ul>
+            <li>
+              <Image src={MafiaVisual} alt="mafia_visual" />
+            </li>
+          </ul>
+        </div>
+        <div className={S.gameTitle}>
+          <h2>Mafia Game</h2>
+          <div className={S.gameButton}>
+            <button>Game Start</button>
+            <button>More Info</button>
+          </div>
+        </div>
+      </section> */}
+      <section className={S.roomSection}>
+        <div className={S.MainGnb}>
+          <p>현재 활성화 되어있는 방</p>
+          <div className={S.roomSearchAndButton}>
+            <div className={S.roomSearch}>
+              <label htmlFor="RoomSearch">방 검색하기</label>
+              <input type="text" id="RoomSearch" />
+            </div>
+            <div className={S.gameGoButton}>
+              <Link href="/">빠른입장</Link>
+              <Link href="/">방 만들기</Link>
+            </div>
+          </div>
+        </div>
+        <ul className={S.roomList}>
+          <li>
+            <Link href="/">
+              <Image src={MafiaItem} alt="room image" />
+              <div className={S.roomTitle}>
+                <h3>방 제목</h3>
+                <div className={S.gameName}>
+                  <p>#마피아</p>
+                  <p>1/5</p>
+                </div>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <Image src={MafiaItem} alt="room image" />
+              <div className={S.roomTitle}>
+                <h3>방 제목</h3>
+                <div className={S.gameName}>
+                  <p>#마피아</p>
+                  <p>1/5</p>
+                </div>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <Image src={MafiaItem} alt="room image" />
+              <div className={S.roomTitle}>
+                <h3>방 제목</h3>
+                <div className={S.gameName}>
+                  <p>#마피아</p>
+                  <p>1/5</p>
+                </div>
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </section>
       <RoomButton />
     </main>
   );
