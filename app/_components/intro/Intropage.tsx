1937dd7ff96505ee23c878557f13a93802008dd6
joo1e <urams79@gmail.com>
2024-04-06 02:38:55 +0900

    [Pull #17] 병합완료

--- app/_components/intro/Intropage.tsx
+++ app/_components/intro/Intropage.tsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import S from '@/app/_style/Intropage/video.module.css'
+import React from "react";
+import S from "@/app/_style/IntroPage/video.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/public/images/logo.svg";
 
-const Intropage = () => {
+const IntroPage = () => {
   return (
     <section className="">
-        <header className="">
-            <div className="header_logo"></div>
-        </header>
-        <video className={S.video} src="https://ktfrmyssyzqmoljohixh.supabase.co/storage/v1/object/public/vedio/intro.mp4?t=2024-04-04T09%3A05%3A53.658Z"muted autoPlay loop></video>
+      <header className="">
+        <Link href="/">
+          <Image src={Logo} alt="아이스크레프트 홈페이지입니다." />
+        </Link>
+      </header>
+      <div>
+        <Link href="/main">GetStarted</Link>
+        <Link href="/login">Login</Link>
+        <div>
+          <p>아직 회원가입을 하지 않으셨으면</p>
+          <Link href="/register">회원가입 하기</Link>
+        </div>
+      </div>
+      <video
+        className={S.video}
+        src="https://ktfrmyssyzqmoljohixh.supabase.co/storage/v1/object/public/vedio/intro.mp4?t=2024-04-04T09%3A05%3A53.658Z"
+        muted
+        autoPlay
+        loop
+      ></video>
     </section>
-  )
-}
+  );
+};
 
-
-
-export default Intropage
+export default IntroPage;
