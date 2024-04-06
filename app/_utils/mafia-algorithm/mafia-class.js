04a39584fdc581ba2d5cd6f0ba6d386e97d82704
baram55 <asd7973@gmail.com>
2024-04-07 06:36:30 +0900

    [Feat] : 시민 클래스 구현

--- app/_utils/mafia-algorithm/mafia-class.js
+++ app/_utils/mafia-algorithm/mafia-class.js
@@ -5,7 +5,33 @@ class Participant {
     this.isReady = false;
     this.index = index;
   }
-  setReady(isReady) {
+  //NOTE - 게임 준비하기
+  ready(isReady) {
     this.isReady = isReady;
+  }
+}
+
+class Citizen extends Participant {
+  constructor(participant) {
+    super(participant.userId, participant.userNickname, participant.index);
+    this.role = "시민";
+    this.isLived = true;
+    this.voteTo = "";
+    this.votedCount = 0;
+  }
+  //NOTE - 플레이어를 선택하는 투표
+  voteToPlayer(citizen) {
+    this.voteTo = receiverIndex;
+    citizen.votedCount++;
+  }
+
+  //NOTE - 찬성, 반대를 결정하는 투표
+  voteYesOrNo(votes, yesOrNo) {
+    votes.push(yesOrNo);
+    return votes;
+  }
+  //NOTE - 방나가기, setRoles 따로 처리하기
+  exit(roles) {
+    this.isLived = false;
   }
 }
