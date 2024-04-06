105aab249c63f10894aecbc34f4b2bce2fd0179a
박서영 <seokachuu@gmail.com>
2024-04-07 02:27:59 +0900

    [Feat #51] intro페이지 header,footer영역 안보이게 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,31 @@
-import { GeistSans } from "geist/font/sans";
+"use client";
+
 import "./globals.css";
 import QueryProvider from "./provider";
 import { ToastContainer } from "react-toastify";
 import Header from "./_components/layout/Header";
 import Footer from "./_components/layout/Footer";
+import { usePathname } from "next/navigation";
+import { metadata } from "@/metadata";
+import Head from "next/head";
 
 const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
 
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "iceCraft",
-  description: "화상 채팅 기반으로한 게임 서비스"
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const showHeaderAndFooter = pathname !== "/";
+
   return (
-    <html lang="ko" className={GeistSans.className}>
+    <html lang="ko">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
       <body>
         <ToastContainer />
-        {/* <Header /> */}
+        {showHeaderAndFooter && <Header />}
         <QueryProvider>{children}</QueryProvider>
-        {/* <Footer /> */}
+        {showHeaderAndFooter && <Footer />}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-// import MainPage from "./main/page";
 import IntroPage from "./_components/intro/IntroPage";
 
 export default async function Index() {
