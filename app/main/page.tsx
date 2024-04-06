fee3452b2b27aaa58108fa32b96a2db5d07dd6b9
박서영 <seokachuu@gmail.com>
2024-04-07 00:47:07 +0900

    [Style #51] header,footer layout 수정

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -9,88 +9,52 @@ import S from "@/app/_style/mainPage/main.module.css";
 const Mainpage = () => {
   return (
     <main className={S.main}>
-      {/* <section className={S.visualSection}>
-        <div className={S.visual}>
-          <ul>
+      <div className={S.mainWrap}>
+        <section className={S.visualSection}>
+          <div className={S.visual}>
+            <ul>
+              <li>
+                <Image src={MafiaVisual} alt="mafia_visual" />
+              </li>
+            </ul>
+          </div>
+          <div className={S.gameTitle}>
+            <h2>Mafia Game</h2>
+            <div className={S.gameButton}>
+              <button>Game Start</button>
+              <button>More Info</button>
+            </div>
+          </div>
+        </section>
+        <section className={S.roomSection}>
+          <div className={S.MainGnb}>
+            <p>현재 활성화 되어있는 방</p>
+            <div className={S.roomSearchAndButton}>
+              <div className={S.roomSearch}>
+                <label htmlFor="RoomSearch">방 검색하기</label>
+                <input type="text" id="RoomSearch" />
+              </div>
+              <div className={S.gameGoButton}>
+                <Link href="/">빠른입장</Link>
+                <Link href="/">방 만들기</Link>
+              </div>
+            </div>
+          </div>
+          <ul className={S.roomList}>
             <li>
-              <Image src={MafiaVisual} alt="mafia_visual" />
+              <Image src={MafiaItem} alt="room image" />
+              <div className={S.roomTitle}>
+                <h3>방 제목</h3>
+                <div className={S.gameName}>
+                  <p>#마피아</p>
+                  <p>1/5</p>
+                </div>
+              </div>
             </li>
           </ul>
-        </div>
-        <div className={S.gameTitle}>
-          <h2>Mafia Game</h2>
-          <div className={S.gameButton}>
-            <button>Game Start</button>
-            <button>More Info</button>
-          </div>
-        </div>
-      </section> */}
-      <section className={S.roomSection}>
-        <div className={S.MainGnb}>
-          <p>현재 활성화 되어있는 방</p>
-          <div className={S.roomSearchAndButton}>
-            <div className={S.roomSearch}>
-              <label htmlFor="RoomSearch">방 검색하기</label>
-              <input type="text" id="RoomSearch" />
-            </div>
-            <div className={S.gameGoButton}>
-              <Link href="/">빠른입장</Link>
-              <Link href="/">방 만들기</Link>
-            </div>
-          </div>
-        </div>
-        <ul className={S.roomList}>
-          <li>
-            <Link href="/">
-              <Image src={MafiaItem} alt="room image" />
-              <div className={S.roomTitle}>
-                <h3>방 제목</h3>
-                <div className={S.gameName}>
-                  <p>#마피아</p>
-                  <p>1/5</p>
-                </div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image src={MafiaItem} alt="room image" />
-              <div className={S.roomTitle}>
-                <h3>방 제목</h3>
-                <div className={S.gameName}>
-                  <p>#마피아</p>
-                  <p>1/5</p>
-                </div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image src={MafiaItem} alt="room image" />
-              <div className={S.roomTitle}>
-                <h3>방 제목</h3>
-                <div className={S.gameName}>
-                  <p>#마피아</p>
-                  <p>1/5</p>
-                </div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image src={MafiaItem} alt="room image" />
-              <div className={S.roomTitle}>
-                <h3>방 제목</h3>
-                <div className={S.gameName}>
-                  <p>#마피아</p>
-                  <p>1/5</p>
-                </div>
-              </div>
-            </Link>
-          </li>
-        </ul>
-      </section>
-      <RoomButton />
+        </section>
+        <RoomButton />
+      </div>
     </main>
   );
 };
