ef4f30a3dcfc39e29c3ae21d96b4e1e6156f7fa6
baram55 <asd7973@gmail.com>
2024-04-07 07:02:49 +0900

    [Feat] : 경찰 클래스 구현

--- app/_utils/mafia-algorithm/mafia-class.js
+++ app/_utils/mafia-algorithm/mafia-class.js
@@ -45,3 +45,19 @@ class Mafia extends Citizen {
     return player;
   }
 }
+
+class Police extends Citizen {
+  constructor(citizen) {
+    super(citizen.userId, citizen.userNickname, citizen.index);
+    this.role = "경찰";
+  }
+
+  //NOTE - 경찰이 플레이어가 마피아 인지 알아냄
+  checkPlayerMafia(player) {
+    if (player.role === "마피아") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
