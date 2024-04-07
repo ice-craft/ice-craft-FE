96388d705d9cbeb790be90f885d9c3e35d6690c4
baram55 <asd7973@gmail.com>
2024-04-07 23:36:38 +0900

    [Add] socket.js 추가

--- app/chat_client/page.tsx
+++ app/chat_client/page.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { socket } from "@/app/_utils/socket/socket";
 
 const ChatClient = () => {
+  useEffect(() => {}, []);
+
+  socket.on("connect", () => {
+    console.log("연결됨");
+  });
+
   return <div>ChatClient</div>;
 };
 
