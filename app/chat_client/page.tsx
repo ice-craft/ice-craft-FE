85043b0def37b96b61107d08ee095b6295db0fd4
baram55 <asd7973@gmail.com>
2024-04-08 00:37:22 +0900

    [Feat] server와 연결했을 때와 연결을 끊었을 때 구현

--- app/chat_client/page.tsx
+++ app/chat_client/page.tsx
@@ -1,12 +1,20 @@
 "use client";
+
 import React, { useEffect } from "react";
 import { socket } from "@/app/_utils/socket/socket";
 
 const ChatClient = () => {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log("first");
+    socket.disconnect();
+  }, []);
 
   socket.on("connect", () => {
     console.log("연결됨");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("연결 종료");
   });
 
   return <div>ChatClient</div>;
