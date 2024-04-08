83416af2b6a19db7573f1d6908490161094657a7
baram55 <asd7973@gmail.com>
2024-04-08 16:37:51 +0900

    [Fix] 카테고리를 마피아로 변경

--- app/_utils/socket/socket.ts
+++ app/_utils/socket/socket.ts
@@ -2,6 +2,6 @@
 
 import { Socket, io } from "socket.io-client";
 
-export const socket: Socket = io("http://localhost:4000", {
+export const socket: Socket = io("http://localhost:4000/mafia", {
   autoConnect: false
 });
