5998b5ddbf98554e77130da08d34dc7cb0804e01
박서영 <seokachuu@gmail.com>
2024-04-08 21:37:29 +0900

    [Feat #77] 직업에 따른 조건부랜더링 추가

--- app/_components/mafia/UserWorkModal.tsx
+++ app/_components/mafia/UserWorkModal.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import S from "@/app/_style/modal/modal.module.css";
 import DoctorCard from "@/public/images/Doctor_Card.png";
 import PoliceCard from "@/public/images/Police_Card.png";
 import MafiaCard from "@/public/images/Mafia_Card.png";
 import CitizensCard from "@/public/images/Citizens_Card.png";
 import Image from "next/image";
+import { Role } from "@/app/_types/index";
 
 const UserWorkModal = () => {
+  const [role, setRole] = useState<Role>("mafia");
+
+  const cards = {
+    doctor: { src: DoctorCard, alt: "의사" },
+    police: { src: PoliceCard, alt: "경찰" },
+    mafia: { src: MafiaCard, alt: "마피아" },
+    citizens: { src: CitizensCard, alt: "시민" }
+  };
+
+  const currentCard = cards[role];
+
   return (
     <>
       <div className={S.modalWrap}>
@@ -14,16 +26,7 @@ const UserWorkModal = () => {
           <h2 className={S.workTitle}>직업을 선택합니다.</h2>
           <ul className={S.workList}>
             <li>
-              <Image src={CitizensCard} alt="doctor" />
-            </li>
-            <li>
-              <Image src={MafiaCard} alt="doctor" />
-            </li>
-            <li>
-              <Image src={PoliceCard} alt="doctor" />
-            </li>
-            <li>
-              <Image src={DoctorCard} alt="doctor" />
+              <Image src={currentCard.src} alt={currentCard.alt} />
             </li>
           </ul>
         </div>
