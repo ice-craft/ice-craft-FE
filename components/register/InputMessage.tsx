85555968f0d5db722b639b1b36411d51a29af85e
박서영 <seokachuu@gmail.com>
2024-04-09 06:54:03 +0900

    [Style #89] 회원가입 페이지 css layout 완료

--- components/register/InputMessage.tsx
+++ components/register/InputMessage.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import S from "@/style/register/register.module.css";
 
 export const InputMessage = ({ text }: { text: string }) => {
-  let textColor = "text-red-500";
+  let error = "#e72424";
 
   switch (text) {
     case "사용 가능한 이메일입니다.":
     case "사용 가능한 닉네임입니다.":
-      textColor = "text-blue-500";
+      error = "#5c5bad";
       break;
   }
 
-  return <p className={textColor}>{text}</p>;
+  return <p className={S.error}>{text}</p>;
 };
