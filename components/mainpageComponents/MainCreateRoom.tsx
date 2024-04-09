f4f256dcb5d98e1ed8b3315b02579028a50e0ce7
joo1e <urams79@gmail.com>
2024-04-09 20:34:52 +0900

    [Feat #38] 조인룸 생성, 임시아이디 생성

--- components/mainpageComponents/MainCreateRoom.tsx
+++ components/mainpageComponents/MainCreateRoom.tsx
@@ -7,13 +7,13 @@ import MafiaGameChoiceActive from "@/public/images/game_choice_mafia_active.png"
 import MafiaGameSong from "@/public/images/game_choice_song.png";
 import MafiaGameSongActive from "@/public/images/game_choice_mafia_song_active.png";
 import Image from "next/image";
-import { createRoom } from "@/utils/supabase/roomAPI";
+import { createRoom, joinRoom } from "@/utils/supabase/roomAPI";
 
 const MainCreateRoom = () => {
   const { setIsModal } = useModalStore();
-  const [selectedGame, setSelectedGame] = useState<string | null>(null);
+  const [selectedGame, setSelectedGame] = useState<string>("마피아");
   const [roomTitle, setRoomTitle] = useState<string>("");
-  const [numberOfPlayers, setNumberOfPlayers] = useState<number>(0);
+  const [numberOfPlayers, setNumberOfPlayers] = useState<number>(-1);
 
   const router = useRouter();
 
@@ -22,12 +22,15 @@ const MainCreateRoom = () => {
       setIsModal(false);
     }
   };
-  const createRoomSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
+
+  const createRoomSubmitHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // 유효성검사필요
     // if (!selectedGame || !roomTitle || !numberOfPlayers) {
     // }
-    createRoom(roomTitle, selectedGame!, numberOfPlayers);
+    const userId = crypto.randomUUID(); //NOTE - 테스트용 코드
+    const { room_id } = await createRoom(roomTitle, selectedGame, numberOfPlayers);
+    await joinRoom(room_id, userId);
     router.push(`/room/${roomTitle}`);
     setIsModal(false);
   };
@@ -35,31 +38,47 @@ const MainCreateRoom = () => {
   return (
     <div className={S.modalWrap} onClick={closeModalHandler}>
       <div className={S.modal}>
-        <div className="MainCreateRoom">
-          <div className="MainCreateRoom-content">
-            <form onSubmit={createRoomSubmitHandler}>
-              <h2>게임을 선택해주세요</h2>
-              <p>게임 고르기</p>
-              <button onClick={() => setSelectedGame("마피아")}>마피아</button>
-              <button onClick={() => setSelectedGame("노래 맞추기")}>노래 맞추기</button>
-              <p>방제목</p>
-              <input type="text" value={roomTitle} onChange={(e) => setRoomTitle(e.target.value)} />
-              <p>인원수</p>
-              <select value={numberOfPlayers || ""} onChange={(e) => setNumberOfPlayers(Number(e.target.value))}>
-                <option value="5">5명</option>
-                <option value="6">6명</option>
-                <option value="7">7명</option>
-                <option value="8">8명</option>
-                <option value="9">9명</option>
-                <option value="10">10명</option>
-              </select>
-              <div>
-                <button onClick={() => setIsModal(false)}>닫기</button>
-                <button type="submit">확인</button>
-              </div>
-            </form>
+        <form onSubmit={createRoomSubmitHandler} className={S.gameForm}>
+          <h2 className={S.gameChoice}>게임을 선택해 주세요</h2>
+          <div>
+            <h3 className={S.gameTitle}>게임 고르기</h3>
+            <ul className={S.gameChoiceList}>
+              <li onClick={() => setSelectedGame("마피아")}>
+                <Image src={MafiaGameChoiceActive} alt="마피아 게임" />
+              </li>
+              <li onClick={() => setSelectedGame("노래 맞추기")}>
+                <Image src={MafiaGameSong} alt="노래 맞추기 게임" />
+              </li>
+            </ul>
           </div>
-        </div>
+          <div className={S.gameNameText}>
+            <h3 className={S.gameTitle}>방 제목</h3>
+            <input
+              type="text"
+              id="RoomName"
+              value={roomTitle}
+              placeholder="방 제목을 입력해 주세요."
+              onChange={(e) => setRoomTitle(e.target.value)}
+            />
+          </div>
+          <div className={S.playerPeopleChoice}>
+            <h3 className={S.gameTitle}>인원수</h3>
+            <select value={numberOfPlayers || ""} onChange={(e) => setNumberOfPlayers(Number(e.target.value))}>
+              <option value="5">5명</option>
+              <option value="6">6명</option>
+              <option value="7">7명</option>
+              <option value="8">8명</option>
+              <option value="9">9명</option>
+              <option value="10">10명</option>
+            </select>
+          </div>
+          <div className={S.gameChoiceButton}>
+            <button className={S.closedButton} onClick={() => setIsModal(false)}>
+              닫기
+            </button>
+            <button type="submit">확인</button>
+          </div>
+        </form>
       </div>
     </div>
   );
