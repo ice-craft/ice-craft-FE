84b694f38ae4260786cdbe3c62a74a009aeebf9c
박서영 <seokachuu@gmail.com>
2024-04-11 23:40:11 +0900

    [Feat #96] progress bar css 수정

--- components/mafia/MafiaModal.tsx
+++ components/mafia/MafiaModal.tsx
@@ -2,7 +2,7 @@ import { useCountDown } from "@/hooks/useCountDown";
 import S from "@/style/modal/modal.module.css";
 
 const MafiaModal = () => {
-  const initialTime = 10;
+  const initialTime = 5;
   const count = useCountDown(initialTime);
 
   return (
@@ -11,7 +11,11 @@ const MafiaModal = () => {
         <div className={S.modal}>
           <div>
             <h1>첫번째 턴</h1>
-            <progress className={S.progress} value={(initialTime - count) * (100 / initialTime)} max={100}></progress>
+            <progress
+              className={S.progress}
+              value={(initialTime * 10 - count) * (100 / (initialTime * 10))}
+              max={100}
+            ></progress>
           </div>
         </div>
       </div>
