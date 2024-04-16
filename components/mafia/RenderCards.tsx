fdb0f064e2e2b1e1170c19c38f7d17fd2265ed4f
박서영 <seokachuu@gmail.com>
2024-04-16 21:30:08 +0900

    [Refactor #131] RenderCards ul태그 제거

--- components/mafia/RenderCards.tsx
+++ components/mafia/RenderCards.tsx
@@ -5,22 +5,20 @@ import { RenderCardsProps } from "@/types/index";
 const RenderCards: React.FC<RenderCardsProps> = ({ cards, role, showAllCards }) => {
   if (showAllCards) {
     return (
-      <ul>
+      <>
         {Object.entries(cards).map(([key, { src, alt }]) => (
           <li key={key}>
             <Image src={src} alt={alt} />
           </li>
         ))}
-      </ul>
+      </>
     );
   } else if (role && cards[role]) {
     const card = cards[role];
     return (
-      <ul>
-        <li>
-          <Image src={card.src} alt={card.alt} />
-        </li>
-      </ul>
+      <li>
+        <Image src={card.src} alt={card.alt} />
+      </li>
     );
   } else {
     return null;
