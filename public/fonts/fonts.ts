135aab9c2a6577751230bc921b20d028dbb1d01c
박서영 <seokachuu@gmail.com>
2024-04-17 00:56:02 +0900

    [Refactor #128] font woff 추가

--- public/fonts/fonts.ts
+++ public/fonts/fonts.ts
@@ -1,9 +1,12 @@
 import localFont from "next/font/local";
 
 export const pretendard = localFont({
-  src: "./PretendardVariable.otf",
-  display: "swap",
-  weight: "400 700"
+  src: [
+    { path: "./PretendardVariable.otf", weight: "400 700" },
+    { path: "./Pretendard-Regular.woff", weight: "400" },
+    { path: "./Pretendard-Bold.woff", weight: "700" }
+  ],
+  display: "swap"
 });
 
 export const designer = localFont({
