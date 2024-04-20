cfd995af3a6aae040fe6b033c632cabed07eacf9
박서영 <seokachuu@gmail.com>
2024-04-20 10:07:22 +0900

    [Feat #170] main loading 애니메이션 추가

--- components/mafia/Loading.tsx
+++ components/mafia/Loading.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import React from "react";
-import LoadingIcon from "@/assets/images/loading.svg";
 import S from "@/style/commons/commons.module.css";
+import { designer } from "@/public/fonts/fonts";
 
 const Loading = () => {
   return (
-    <section className={S.loading}>
-      <Image src={LoadingIcon} alt="Loading" className={S.loadingAnimation} />
+    <section className={`${S.loadingWrapper} ${designer.className}`}>
+      <div>IceCraft</div>
+      <div>Loading...</div>
+      <div>메인페이지로 이동중 입니다.</div>
     </section>
   );
 };
