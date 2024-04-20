65471858adb7faa08636aa38295fc2cfa01fe2e5
박서영 <seokachuu@gmail.com>
2024-04-20 09:34:12 +0900

    [Feat #170] loading html 추가

--- components/mafia/Loading.tsx
+++ components/mafia/Loading.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import React from "react";
-import Loading from "@/assets/images/loading.svg";
+import LoadingIcon from "@/assets/images/loading.svg";
+import S from "@/style/commons/commons.module.css";
 
 const Loading = () => {
   return (
-    <section className={}>
-      <Image src={} alt="Loading" />
+    <section className={S.loading}>
+      <Image src={LoadingIcon} alt="Loading" />
     </section>
   );
 };
