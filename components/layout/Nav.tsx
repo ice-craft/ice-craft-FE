e3b7e6347af4166114bca8b3d130e0a1579d89e5
박서영 <seokachuu@gmail.com>
2024-04-21 21:47:34 +0900

    [Feat #179] nav로그아웃 연결

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,21 +1,42 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import S from "@/style/commons/commons.module.css";
-import { getUserNickname, logOut } from "@/utils/supabase/authAPI";
+import { logOut } from "@/utils/supabase/authAPI";
+import useConnectStore from "@/store/connect-store";
 
 const Nav = () => {
+  const { userId, nickname } = useConnectStore();
+  console.log(userId, nickname);
+
+  const logoutHandler = () => {
+    logOut();
+  };
+
   return (
     <nav>
       <ul className={S.ul}>
         <li>
           <Link href="/ranking">랭킹</Link>
         </li>
-        <li>
-          <Link href="/register">회원가입</Link>
-        </li>
-        <li>
-          <Link href="/login">로그인</Link>
-        </li>
+        {userId ? (
+          <>
+            <li>{nickname} 님 환영합니다.</li>
+            <li>
+              <button onClick={logoutHandler}>로그아웃</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link href="/register">회원가입</Link>
+            </li>
+            <li>
+              <Link href="/login">로그인</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
