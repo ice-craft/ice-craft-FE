3ddcacd7c94b4f879e5a81ec525c5f3fb694522b
이준구 <leejungoo1396@gmail.com>
2024-04-27 02:31:51 +0900

    [Fix #215] 특정 유저의 카메라 제어 기능 수정

--- utils/mafiaSocket/mafiaSocket.ts
+++ utils/mafiaSocket/mafiaSocket.ts
@@ -1,9 +1,10 @@
 import { MediaState, ShowModalComponents } from "@/types";
-import { TrackReferenceOrPlaceholder, useLocalParticipant, useParticipantTracks } from "@livekit/components-react";
+import { TrackReferenceOrPlaceholder } from "@livekit/components-react";
 import { allMediaSetting, specificUserVideoSetting } from "../participantCamSettings/camSetting";
 import { socket } from "../socket/socket";
 import { setStatus } from "../supabase/statusAPI";
 
+//NOTE - 게임 시작
 export const r0NightStartHandler = async ({
   roomId,
   userId,
@@ -16,12 +17,12 @@ export const r0NightStartHandler = async ({
 }: ShowModalComponents) => {
   console.log("r0NightStart 수신");
 
-  //NOTE - 모달창 요소
+  // 모달창 요소
   setIsOpen(true);
   setTitle("게임 시작");
   setMessage("누군가 당신의 뒤를 노리고 있습니다.");
   setTimer(5);
-  setIsOverlay(false); //클릭 이벤트 비활성화
+  setIsOverlay(false); //캠 클릭 이벤트 비활성화
 
   // 5초 후에 setStatus와 socket.emit 실행
   const r0NightStartTimer = setTimeout(async () => {
@@ -34,6 +35,7 @@ export const r0NightStartHandler = async ({
   setTimerIds((prevTimerIds) => [...prevTimerIds, r0NightStartTimer]);
 };
 
+//NOTE - 카메라 및 마이크 off
 export const r0TurnAllUserCameraMikeOffHandler = async (
   tracks: TrackReferenceOrPlaceholder[],
   userId: string,
@@ -49,6 +51,7 @@ export const r0TurnAllUserCameraMikeOffHandler = async (
   console.log("r0TurnAllUserCameraMikeOff 송신");
 };
 
+//NOTE - UI: 마피아 유저에게 "마피아들은 고개를 들어 서로를 확인해 주세요." 모달창 띄우기
 export const r0ShowMafiaUserEachOther = async ({
   roomId,
   userId,
@@ -61,12 +64,12 @@ export const r0ShowMafiaUserEachOther = async ({
 }: ShowModalComponents) => {
   console.log("r0ShowMafiaUserEachOther 수신");
 
-  //NOTE - 모달창 요소
+  //모달창 요소
   setIsOpen(true);
   setTitle("마피아 시간");
   setMessage("클클클... 누군가를 죽여야한다니 흥미롭군, 나의 파트너는 누구지?");
   setTimer(5);
-  setIsOverlay(false); //클릭 이벤트 비활성화
+  setIsOverlay(false); //캠 클릭 이벤트 비활성화
 
   // 5초 후에 setStatus와 socket.emit 실행
   const r0ShowMafiaUserEachOtherTimer = setTimeout(async () => {
@@ -79,31 +82,39 @@ export const r0ShowMafiaUserEachOther = async ({
   setTimerIds((prevTimerIds) => [...prevTimerIds, r0ShowMafiaUserEachOtherTimer]);
 };
 
-//NOTE -  마피아 유저 화상 카메라와 마이크만 켬
+//NOTE -  마피아 유저의 캠 및 오디오 ON
 export const r0TurnMafiaUserCameraOnHandler = async ({
   tracks,
   localUserId,
-  specificUser,
+  remoteParticipants,
+  players,
   userId,
   roomId,
   setTimerIds
 }: MediaState) => {
   console.log("r0TurnMafiaUserCameraOn 수신");
 
-  console.log("localUserId", localUserId);
-  console.log("specificUser", specificUser);
   allMediaSetting(tracks, false);
-  //NOTE -  1) 특정 유저의 track을 받아온다.
-  // specificUser
-  //NOTE -  2) 현재 방의 유저 중에 특정 user인지를 파악한다.
-  if (!localUserId || !specificUser) {
-    return;
+  // console.log("localUserId", localUserId);
+  // console.log("userId", userId);
+  // console.log("RemoteParticipants", RemoteParticipants);
+  // console.log("players", players);
+
+  // 1) localUserId, specificUser, players의 값이 null이 아닐 경우에만 수행
+  if (!localUserId || !remoteParticipants || !players) {
+    return () => {};
   }
 
-  if (localUserId === userId) {
-    //NOTE -  3) 해당 특정 유저일 경우 track 및 boolean값을 통해 캠 활성화 및 비활성화
-    specificUserVideoSetting(specificUser, true);
-  }
+  // 2) 특정 유저의 track 및 boolean값을 통해 캠 활성화
+  const MafiaUserId = players.map((userId) => {
+    return remoteParticipants.map((remote) => {
+      if (remote.metadata === userId && localUserId === userId) {
+        return remote;
+      }
+    });
+  });
+  console.log("MafiaUserId", MafiaUserId);
+  // specificUserVideoSetting(MafiaUserId, true)
 
   // 5초 후에 setStatus와 socket.emit 실행
   const r0TurnMafiaUserCameraOnTimer = setTimeout(async () => {
@@ -116,11 +127,12 @@ export const r0TurnMafiaUserCameraOnHandler = async ({
   setTimerIds((prevTimerIds: any) => [...prevTimerIds, r0TurnMafiaUserCameraOnTimer]);
 };
 
-//NOTE -  마피아 유저 화상 카메라와 마이크만 켬
+//NOTE -  마피아 유저의 캠 및 오디오 Off
 export const r0TurnMafiaUserCameraOffHandler = async ({
   tracks,
   localUserId,
-  specificUser,
+  remoteParticipants,
+  players,
   userId,
   roomId,
 
@@ -128,13 +140,20 @@ export const r0TurnMafiaUserCameraOffHandler = async ({
 }: MediaState) => {
   console.log("r0TurnMafiaUserCameraOff 수신");
 
-  allMediaSetting(tracks, false);
-  //NOTE -  1) 특정 유저의 track을 받아온다.
-  //NOTE -  2) 현재 방의 유저 중에 특정 user인지를 파악한다.
-  if (localUserId === userId) {
-    //NOTE -  3) 해당 특정 유저일 경우 track 및 boolean값을 통해 캠 활성화 및 비활성화
-    specificUserVideoSetting(specificUser, false);
+  if (!localUserId || !remoteParticipants || !players) {
+    return () => {};
   }
+  allMediaSetting(tracks, false);
+
+  // 2) 특정 유저의 track 및 boolean값을 통해 캠 활성화
+  const MafiaUserId = players.map((userId) => {
+    return remoteParticipants.map((remote) => {
+      if (remote.metadata === userId && localUserId === userId) {
+        return remote;
+      }
+    });
+  });
+  console.log("MafiaUserId", MafiaUserId);
 
   // 5초 후에 setStatus와 socket.emit 실행
   const r0TurnMafiaUserCameraOffTimer = setTimeout(async () => {
