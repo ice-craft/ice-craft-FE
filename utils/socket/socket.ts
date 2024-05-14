8f4093d37f086915ee0a20b93e29903f86af781b
LeeJnuGoo <leejungoo1396@gmail.com>
2024-05-14 21:44:49 +0900

    [Test #234] 임시 socket 변경

--- utils/socket/socket.ts
+++ utils/socket/socket.ts
@@ -2,6 +2,6 @@
 
 import { Socket, io } from "socket.io-client";
 
-export const socket: Socket = io("http://43.201.76.17:4000/mafia", {
+export const socket: Socket = io("http://localhost:4000/mafia", {
   autoConnect: false
 });
