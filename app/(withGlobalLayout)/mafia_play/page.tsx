825acfa437187f2db9f313ea4bad0ffa8b2c9680
baram <asd7973@gmail.com>
2024-05-16 02:48:38 +0900

    [Fix #235] test 코드에서 roomId를 송신하도록 수정

--- app/(withGlobalLayout)/mafia_play/page.tsx
+++ app/(withGlobalLayout)/mafia_play/page.tsx
@@ -60,7 +60,7 @@ const MafiaPlay = () => {
           break;
         case "testStart":
           console.log(eventName, "테스트 시작");
-          socket.emit(eventName, "테스트 시작");
+          socket.emit(eventName, roomId.current);
           break;
       }
     } else {
