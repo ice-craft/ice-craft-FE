66c7a6545a0d20bd977c6397baab2cdf753edde9
박서영 <seokachuu@gmail.com>
2024-05-21 00:59:49 +0900

    [Feat#240] 버튼 모달 root 사이즈 변경

--- components/modal/VoteResultModal.tsx
+++ components/modal/VoteResultModal.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useCountDown } from "@/hooks/useCountDown";
+import useShowModalStore from "@/store/showModal.store";
+import S from "@/style/modal/modal.module.css";
 
 const VoteResultModal = () => {
-  return <div>결과값 모달</div>;
+  const { isOpen, timer, title, message, nickname } = useShowModalStore();
+  const count = useCountDown(timer);
+
+  if (count === 0) {
+    return null;
+  }
+
+  if (!isOpen) return null;
+
+  return (
+    <>
+      <div className={S.modalWrap}>
+        <div className={S.modal}>
+          <div>
+            {title && <h1>{title}</h1>}
+            {message && <h2>{message}</h2>}
+            {nickname && <h2>{nickname}</h2>}
+            <progress className={S.progress} value={(timer * 10 - count) * (100 / (timer * 10))} max={100}></progress>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default VoteResultModal;
