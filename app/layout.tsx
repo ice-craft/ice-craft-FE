7b6d25794d21b1fa224a5f94f6fe17bb345d8548
박서영 <seokachuu@gmail.com>
2024-05-22 13:50:31 +0900

    [Fix#240] root 메타데이터 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,16 +34,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" className={pretendard.className}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta property="og:title" content="Ice Craft" />
-        <meta property="og:description" content="into stunning space! 놀라운 공간 속으로!" />
-        {/* //NOTE - URL<meta property="og:url" content="" /> */}
-        <meta property="og:type" content="game" />
-        {/* //NOTE - 대표이미지<meta property="og:image" content="" /> */}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
       <body>
         <ToastContainer position="top-left" />
         <QueryProvider>{children}</QueryProvider>
