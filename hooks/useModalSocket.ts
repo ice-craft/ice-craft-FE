a46cc0662eb557c97285f0155f90d79518afcb3c
LeeJnuGoo <leejungoo1396@gmail.com>
2024-06-04 12:03:01 +0900

    [Fix #262] useModalSocket 주석 추가

--- hooks/useModalSocket.ts
+++ hooks/useModalSocket.ts
@@ -19,9 +19,8 @@ const useModalSocket = () => {
 
   // 추후 "isModalOpen" 통일될 예정
   const sockets = {
-    //NOTE - GroupModal, CheckModal
     showModal: (title: string, timer: number) => {
-      //NOTE - 최후의 투표 모달창 요소
+      //NOTE -  CheckModal(찬성/반대) 투표 모달창 요소
       if (title.includes("찬성/반대 투표")) {
         setCheckIsOpen(true);
         setTitle(title);
@@ -29,23 +28,25 @@ const useModalSocket = () => {
         return;
       }
 
-      //기본 모달창 요소
+      //NOTE - GroupModal 모달창 요소
       setGroupIsOpen(true);
       setTitle(title);
       setTimer(timer);
     },
 
-    //NOTE - UserRoleModal
+    //NOTE - UserRoleModal 모달창 요소
     showAllPlayerRole: (role: Role, timer: number) => {
       setRoleIsOpen(true);
       setRole(role);
       setTimer(timer);
     },
+    //NOTE - 투표 결과 모달창 요소
     showVoteResult: (voteResult: VoteResult[], timer: number) => {
       setVoteIsOpen(true);
       setVoteResult(voteResult);
       setTimer(timer);
     },
+    //NOTE - 찬성/반대 투표 결과 모달창 요소
     showVoteDeadOrLive: (voteResult: YesOrNoResults, timer: number) => {
       setVoteIsOpen(true);
       setYesOrNoVoteResult(voteResult);
