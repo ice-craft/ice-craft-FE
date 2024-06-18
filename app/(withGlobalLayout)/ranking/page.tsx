ccde005222361ac16f64737889035ee2a38cbd87
AhnJUWon <urams79@gmail.com>
2024-06-19 01:37:53 +0900

    [Fix #286] 병합완료

--- app/(withGlobalLayout)/ranking/page.tsx
+++ app/(withGlobalLayout)/ranking/page.tsx
@@ -8,6 +8,7 @@ import GoTopButton from "@/utils/GoTopButton";
 import { createClient } from "@/utils/supabase/server";
 import Image from "next/image";
 import SearchIcon from "@/assets/images/icon_search.svg";
+import Pagination from "@/components/ranking/Pagination";
 
 const Rankingpage = async () => {
   const supabase = createClient();
@@ -44,7 +45,7 @@ const Rankingpage = async () => {
               </div>
             </li>
           </ul>
-          <ul className={S.userRankingList}>
+          {/* <ul className={S.userRankingList}>
             {data.map((item: any, index: number) => (
               <li key={index}>
                 <div>
@@ -56,14 +57,14 @@ const Rankingpage = async () => {
                 </div>
               </li>
             ))}
-          </ul>
+          </ul> */}
         </div>
       ) : (
         <div className={S.rankingEmpty}>
           <Image src={RankingEmptyImage} alt="랭킹페이지 내용이 없습니다." />
         </div>
       )}
-      <div className={S.pagerWrapper}>
+      {/* <div className={S.pagerWrapper}>
         <button>
           <Image src={ArrowLeft} alt="left button" />
         </button>
@@ -78,7 +79,8 @@ const Rankingpage = async () => {
         <button>
           <Image src={ArrowRight} alt="right button" />
         </button>
-      </div>
+      </div> */}
+      <Pagination data={data} />
       <GoTopButton />
     </section>
   );
---
+++ components/ranking/Pagination.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
+import S from "@/style/ranking/ranking.module.css";
+import Image from "next/image";
+import ArrowLeft from "@/assets/images/ranking_arrow_left.svg";
+import ArrowRight from "@/assets/images/ranking_arrow_right.svg";
+
+function Items({ currentItems }: any) {
+  return (
+    <>
+      {currentItems && (
+        <ul className={S.userRankingList}>
+          {currentItems.map((item: any, index: number) => (
+            <li key={index}>
+              <div>
+                <h2>{index + 1}</h2>
+                <h3>{item.nickname}</h3>
+                <p className={S.mafiaUserRanking}>{item.game_category}</p>
+                <p className={S.songUserRanking}>-</p>
+                <p className={S.totalRanking}>{item.total_score}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+
+function PaginatedItems({ items, itemsPerPage }: any) {
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+
+  const handlePageClick = (event: any) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <Items currentItems={currentItems} />
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel=">"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="<"
+        renderOnZeroPageCount={null}
+      />
+    </>
+  );
+}
+
+export default function Pagination() {
+  const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  return (
+    <div>
+      <PaginatedItems items={items} itemsPerPage={10} />
+
+      {/* <div className={S.pagerWrapper}>
+        <button>
+          <Image src={ArrowLeft} alt="left button" />
+        </button>
+        <ol className={S.pager}>
+          <li className={S.active}>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+        </ol>
+        <button>
+          <Image src={ArrowRight} alt="right button" />
+        </button>
+      </div> */}
+    </div>
+  );
+}
