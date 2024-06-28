cd709babfddc21698416c1bfb6445cf63724bff2
LeeJnuGoo <leejungoo1396@gmail.com>
2024-06-29 00:13:57 +0900

    [Feat #309] 게임 시작 및 게임 종료 시 게임 초기화

--- components/mafia/MafiaPlayRooms.tsx
+++ components/mafia/MafiaPlayRooms.tsx
@@ -1,24 +1,36 @@
+import useGameStateSocket from "@/hooks/useGameStateSocket";
 import useMediaSocket from "@/hooks/useMediaSocket";
 import useSelectSocket from "@/hooks/useSelectSocket";
 import useSocketOn from "@/hooks/useSocketOn";
 import { useExitAction } from "@/store/exit-store";
 import { useGameActions } from "@/store/game-store";
+import { useOverLayActions } from "@/store/overlay-store";
 import S from "@/style/livekit/livekit.module.css";
+import getPlayerNumber from "@/utils/mafiaSocket/getPlayerNumber";
 import { allAudioSetting } from "@/utils/participantCamSettings/camSetting";
 import { socket } from "@/utils/socket/socket";
-import { DisconnectButton, RoomAudioRenderer, useLocalParticipant, useTracks } from "@livekit/components-react";
+import {
+  DisconnectButton,
+  RoomAudioRenderer,
+  useLocalParticipant,
+  useParticipants,
+  useTracks
+} from "@livekit/components-react";
 import { Track } from "livekit-client";
+import { useEffect } from "react";
 import LocalParticipant from "./LocalParticipant";
 import MafiaModals from "./MafiaModals";
 import MafiaToolTip from "./MafiaToolTip";
 import RemoteParticipant from "./RemoteParticipant";
 
 const MafiaPlayRooms = () => {
+  const participants = useParticipants();
   const { localParticipant } = useLocalParticipant();
   const roomId = localParticipant.metadata;
   const userId = localParticipant.identity;
-
-  const { setDiedPlayer } = useGameActions();
+  const isGameState = useGameStateSocket();
+  const { setDiedPlayer, setPlayersNumbers } = useGameActions();
+  const { setReadyPlayers } = useOverLayActions();
   const { setIsExit } = useExitAction();
 
   useMediaSocket(); // 카메라 및 오디오 처리
@@ -33,14 +45,28 @@ const MafiaPlayRooms = () => {
     { onlySubscribed: false } // 구독 여부 상관없이 실행
   );
 
-  //NOTE - 죽은 playerId 관리
   const sockets = {
+    //전체 players의 실시간 Ready 상태
+    setReady: (playerId: string, isReady: boolean) => {
+      setReadyPlayers(playerId, isReady);
+    },
+
+    //죽은 playerId 관리
     diedPlayer: (playerId: string) => {
       setDiedPlayer(playerId);
     }
   };
-  // NOTE - socket On 담당
+
   useSocketOn(sockets);
+
+  //NOTE - 게임 시작 시, 전체 players[] 목록을 정렬 및 번호를 부여
+  useEffect(() => {
+    if (isGameState) {
+      const gamePlayers = getPlayerNumber(participants);
+
+      setPlayersNumbers(gamePlayers);
+    }
+  }, [isGameState]);
 
   //NOTE - 방 나가기 이벤트 헨들러
   // supabase의 성능문제를 해결하기위해 로딩창을 띄어 텀을 주었다.
@@ -51,7 +77,7 @@ const MafiaPlayRooms = () => {
 
   return (
     <section className={S.section}>
-      <LocalParticipant tracks={tracks} />
+      <LocalParticipant tracks={tracks} isGameState={isGameState} />
       <RemoteParticipant tracks={tracks} />
       {/* 원격 참가자의 오디오 트랙을 처리 및 관리 */}
       <RoomAudioRenderer muted={false} />
--- store/image-store.ts
+++ store/image-store.ts
@@ -1,13 +1,12 @@
-import { create } from "zustand";
-import { ImageState } from "../types";
-import { StaticImageData } from "next/image";
-import CamCheck from "@/assets/images/cam_check.svg";
+// import { create } from "zustand";
+// import { ImageState } from "../types";
+// import { StaticImageData } from "next/image";
+// import CamCheck from "@/assets/images/cam_check.svg";
 
-const useCamClickImageState = create<ImageState>((set) => ({
-  imageState: CamCheck as StaticImageData | null,
-  setImageState: (newImage: StaticImageData | null) => set({ imageState: newImage }),
-  setImageReset: () => set({ imageState: CamCheck })
-}));
+// const useCamClickImageState = create<ImageState>((set) => ({
+//   imageState: CamCheck as StaticImageData | null,
+//   setImageState: (newImage: StaticImageData | null) => set({ imageState: newImage }),
+//   setImageReset: () => set({ imageState: CamCheck })
+// }));
 
-export const useJobImageState = () => useCamClickImageState((state) => state.imageState);
-export const useJobImageAction = () => useCamClickImageState((state) => state.setImageState);
+// export const useJobImageAction = () => useCamClickImageState((state) => state.setImageState);
