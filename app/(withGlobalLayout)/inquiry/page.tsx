af7de8f37bd1f79c864f39ff50559938a9f987dd
박서영 <seokachuu@gmail.com>
2024-06-30 18:34:15 +0900

    [Feat #311] CSS요소 추가, 이메일 수정

--- app/(withGlobalLayout)/inquiry/page.tsx
+++ app/(withGlobalLayout)/inquiry/page.tsx
@@ -1,47 +1,69 @@
-import React, { useRef } from "react";
-import S from "@/style/commons/commons.module.css";
-import { SMTPClient } from 'emailjs';
+"use client";
 
-const client = new SMTPClient({
-	user: 'user',
-	password: 'password',
-	host: 'smtp.your-email.com',
-	ssl: true,
-});
+import React, { useRef, useState } from "react";
+import S from "@/style/commons/commons.module.css";
+import emailjs from "emailjs-com";
+import { toast } from "react-toastify";
 
 const InquiryPage = () => {
-  const form = useRef();
+  const form = useRef<HTMLFormElement>(null);
+  const [isSending, setIsSending] = useState(false);
 
-  emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
-  .then((result:any) => {
-      console.log(result.text);
-      alert("문의가 성공적으로 전송되었습니다!");
-  }, (error:any) => {
-      console.log(error.text);
-      alert("문의 전송 중 오류가 발생했습니다. 다시 시도해주세요.");
-  });
-};
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!isSending && form.current) {
+      setIsSending(true);
+
+      emailjs
+        .sendForm(
+          process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID!,
+          process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID!,
+          form.current,
+          process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY!
+        )
+        .then(
+          (result) => {
+            console.log(result);
+            toast.success("문의가 성공적으로 전송되었습니다.");
+            form.current?.reset();
+          },
+          (error) => {
+            console.log(error.text);
+            toast.error("이메일 전송이 실패되었습니다. 다시 시도해 주세요.");
+          }
+        )
+        .finally(() => {
+          setIsSending(false);
+        });
+    }
+  };
 
   return (
     <section className={S.inquiryWrap}>
       <h2>문의사항</h2>
-      <form ref={form} onSubmit={sendEmail}>
-        <label>
-          답변 받으실 이메일
-          <input type="email" name="user_email" placeholder="ex)abc@example.com" required />
-        </label>
-
-        <label>
-          문의 제목
-          <input type="text" name="ask_title" placeholder="제목을 입력해주세요.(20자 이내)" maxLength={20} required />
-        </label>
-
-        <label>
-          문의 내용
-          <textarea name="ask_message" placeholder="제목을 입력해주세요." required />
-        </label>
-
-        <button type="submit" value="문의하기" />
+      <form ref={form} onSubmit={sendEmail} className={S.sendInner}>
+        <div>
+          <label>
+            답변 받으실 이메일 <span>*</span>
+          </label>
+          <input type="email" name="user_email" placeholder="ex) abc@example.com" autoFocus required />
+        </div>
+        <div>
+          <label>
+            제목 <span>*</span>
+          </label>
+          <input type="text" name="title" placeholder="제목을 입력해주세요.(20자 이내)" maxLength={20} required />
+        </div>
+        <div>
+          <label>
+            내용 <span>*</span>
+          </label>
+          <textarea name="message" placeholder="내용을 입력해주세요." required />
+        </div>
+        <button type="submit" disabled={isSending}>
+          보내기
+        </button>
       </form>
     </section>
   );
