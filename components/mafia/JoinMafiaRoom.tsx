0acf63e679715164fe773b180ee71529d8def808
박서영 <seokachuu@gmail.com>
2024-07-03 22:13:08 +0900

    [Pull #321] 병합 완료

--- components/mafia/JoinMafiaRoom.tsx
+++ components/mafia/JoinMafiaRoom.tsx
@@ -1,13 +1,8 @@
 import MafiaPlayRooms from "@/components/mafia/MafiaPlayRooms";
 import usePopStateHandler from "@/hooks/usePopStateHandler";
 import { useGetToken } from "@/hooks/useToken";
-<<<<<<< HEAD
-import { useConnectActions, useNickname, useRoomId, useRoomsCurrent, useUserId } from "@/store/connect-store";
-import { useExitAction } from "@/store/exit-store";
-=======
 import { useNickname, useRoomId, useUserId } from "@/store/connect-store";
 import { useRoomAction } from "@/store/room-store";
->>>>>>> dc1d3a925cd64844345df35a242a5de6648c55d1
 import S from "@/style/livekit/livekit.module.css";
 import useBeforeUnloadHandler from "@/utils/reload/useBeforeUnloadHandler";
 import { socket } from "@/utils/socket/socket";
@@ -22,18 +17,9 @@ const JoinMafiaRoom = () => {
 
   const [isJoinError, setIsJoinError] = useState(true);
   const [isJoin, setIsJoin] = useState(false);
-<<<<<<< HEAD
-  const { setIsExit, setIsBack } = useExitAction();
-  const isBack = usePopStateHandler();
-  const { setRooms } = useConnectActions();
-  const rooms = useRoomsCurrent();
-
-  // BeforeUnloadHandler();
-=======
   const { setIsEntry, setIsBack } = useRoomAction();
   const isPopState = usePopStateHandler();
   const { setIsReLoad } = useBeforeUnloadHandler();
->>>>>>> dc1d3a925cd64844345df35a242a5de6648c55d1
 
   //NOTE - 뒤로가기 시 작동
   useEffect(() => {
@@ -44,15 +30,6 @@ const JoinMafiaRoom = () => {
     }
   }, [isPopState]);
 
-<<<<<<< HEAD
-  // // exitRoom 이벤트 처리
-  // useEffect(() => {
-  //   socket.on("exitRoom", (rooms) => {
-  //     setRooms(rooms); // 방 정보 업데이트
-  //   });
-  // }, [setRooms]);
-
-=======
   //NOTE - 방입장 이벤트
   const joinRoomHandler = () => {
     setIsReLoad(true); //새로고침 팝업창 on, off
@@ -60,7 +37,6 @@ const JoinMafiaRoom = () => {
   };
 
   //NOTE - 방에러 이벤트
->>>>>>> dc1d3a925cd64844345df35a242a5de6648c55d1
   const joinError = (error: Error | string) => {
     // setIsJoinError(false);  // 미디어 비활성화 및 토큰 발급 error시 실행
     console.log("joinError", error);
@@ -81,17 +57,13 @@ const JoinMafiaRoom = () => {
     return (
       <div>
         <p>
-          게임 접속에 불편을 드려서 죄송합니다. 현재 원활한 게임이 진행되지 않고 있으니, 다시 접속해 주시기 바랍니다.
+          게임 접속에 불편을 드려서 죄송합니다. 현재 원활한 게임이 진행되지 않고 있으니, 나갔다 다시 접속해 주시기
+          바랍니다.
         </p>
         <button
           onClick={() => {
             socket.emit("exitRoom", roomId, userId);
-<<<<<<< HEAD
-            setIsExit(true);
-            // setRooms(rooms);
-=======
             setIsEntry(false);
->>>>>>> dc1d3a925cd64844345df35a242a5de6648c55d1
           }}
         >
           나가기
