a178f7c3985a4242366da871d154e79818118b5a
박서영 <seokachuu@gmail.com>
2024-07-25 23:33:03 +0900

    [Style #342] 현재까지 한 내용 커밋

--- components/mafia/JoinMafiaRoom.tsx
+++ components/mafia/JoinMafiaRoom.tsx
@@ -111,6 +111,7 @@ const JoinMafiaRoom = () => {
           audio={true}
           onError={joinErrorHandler}
           connect={true}
+          simulateParticipants={5}
         >
           <MafiaPlayRooms />
         </LiveKitRoom>
