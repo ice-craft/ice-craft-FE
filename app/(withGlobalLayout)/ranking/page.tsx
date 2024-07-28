95589af912726abf58de2007c6028202ed4751ed
baram55 <asd7973@gmail.com>
2024-07-28 21:12:19 +0900

    [Fix #351] FormSearch 제거

--- app/(withGlobalLayout)/ranking/page.tsx
+++ app/(withGlobalLayout)/ranking/page.tsx
@@ -1,5 +1,4 @@
 export const revalidate = 1; //NOTE - 30분
-// "use client";
 
 import RankingEmptyImage from "@/assets/images/ranking_empty.svg";
 import S from "@/style/ranking/ranking.module.css";
@@ -14,7 +13,6 @@ import { getUserInfo } from "@/utils/supabase/authAPI";
 
 const Rankingpage = async () => {
   const rankingList = await getUsersRanking();
-  let myLanking = null;
   console.log("렌더링");
 
   const setRanking = async (rankingList: Ranking[]) => {
@@ -33,32 +31,12 @@ const Rankingpage = async () => {
     }
   };
 
-  const getMyLanking = async () => {
-    // let userInfo = null;
-    try {
-      // userInfo = await getUserInfo();
-
-      // const nickname = userInfo.user_metadata.name;
-      const nickname = "김명환";
-      const result = rankingList.find((ranking) => ranking.nickname === nickname);
-
-      return result;
-    } catch (e) {
-      console.log((e as Error).message);
-      return null;
-    }
-  };
-
   setRanking(rankingList);
-  myLanking = await getMyLanking();
-  console.log("나의 랭킹", myLanking);
-  console.log("랭킹 목록", rankingList);
 
   return (
     <section className={S.sectionWrapper}>
       <div className={S.userRanking}>
         <h2>랭킹 순위</h2>
-        <FormSearch placeholder="닉네임을 입력해 주세요." />
       </div>
       <ul className={S.userRankingTitle}>
         <li>랭킹</li>
@@ -67,7 +45,7 @@ const Rankingpage = async () => {
         <li>노래 맞추기</li>
         <li>총점</li>
       </ul>
-      {myLanking && <MyLanking ranking={myLanking} />}
+      {<MyLanking rankingList={rankingList} />}
       {rankingList ? (
         <Pagination rankingList={rankingList} />
       ) : (
