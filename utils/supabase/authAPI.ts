ccaf5b8ac5f6ec0b959552504f9d9a555b4806de
AhnJUWon <urams79@gmail.com>
2024-07-30 01:05:21 +0900

    [Pull #308] 병합완료

--- utils/supabase/authAPI.ts
+++ utils/supabase/authAPI.ts
@@ -18,18 +18,6 @@ export const checkUserLogIn = async () => {
 export const checkUserLoginInfo = async () => {
   const { data, error } = await supabase.auth.getUser();
 
-  if (error) {
-    throw new Error("로그인 확인에 실패했습니다.");
-  }
-
-  if (data.user) {
-    return data.user;
-  }
-};
-
-export const checkUserLoginInfo = async () => {
-  const { data, error } = await supabase.auth.getUser();
-
   if (data.user) {
     return data.user;
   }
