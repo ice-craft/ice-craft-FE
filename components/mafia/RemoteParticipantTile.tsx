b5672484d70e0ecc84b2fde9e4a6e82340e098c2
LeeJnuGoo <leejungoo1396@gmail.com>
2024-07-31 20:21:12 +0900

    [Fix #359] RemoteParticipant.tsx의 실시간 방장 업데이트

--- components/mafia/RemoteParticipantTile.tsx
+++ components/mafia/RemoteParticipantTile.tsx
@@ -3,13 +3,16 @@ import ChiefImage from "@/assets/images/leader.svg";
 import PlayerDieImages from "@/assets/images/player_die.svg";
 import useClickHandler from "@/hooks/useClickHandler";
 import usePlayerNumber from "@/hooks/usePlayerNumber";
-import { useDiedPlayer, useGameState } from "@/store/game-store";
+import { useChiefPlayer, useDiedPlayer, useGameState } from "@/store/game-store";
 import { useActivePlayer, useIsRemoteOverlay, useJobImageState, useReadyPlayers } from "@/store/overlay-store";
 import S from "@/style/livekit/livekit.module.css";
 import { ParticipantTile, ParticipantTileProps, useEnsureTrackRef } from "@livekit/components-react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const RemoteParticipantTile = ({ trackRef }: ParticipantTileProps) => {
+  const [isChief, setIsChief] = useState(false);
+
   //NOTE - livekit Hooks
   const remote = useEnsureTrackRef(trackRef);
 
@@ -20,28 +23,40 @@ const RemoteParticipantTile = ({ trackRef }: ParticipantTileProps) => {
   const remoteReadyStates = useReadyPlayers();
   const isRemoteOverlay = useIsRemoteOverlay();
   const imageState = useJobImageState();
+  const chiefPlayerId = useChiefPlayer();
 
   //NOTE - custom Hooks
   const { clickHandler } = useClickHandler();
   const playerNumber = usePlayerNumber(remote.participant.identity, isGameState);
 
-  const diedPlayer = diedPlayers.find((diedPlayer) => diedPlayer === remote.participant.identity);
+  const isDiedPlayer = diedPlayers.find((diedPlayer) => diedPlayer === remote.participant.identity);
+
+  //NOTE - 게임 시작 전) 실시간 방장 정보 update
+  useEffect(() => {
+    if (isGameState === "gameReady" && remote.participant.identity === chiefPlayerId.chief) {
+      console.log("Remote 게임 준비 시 방장 UI 실행");
+      setIsChief(true);
+    }
+
+    if (isGameState === "gameStart" || isGameState === "gameEnd") {
+      console.log("Remote 게임 시작 및 종료 후 방장 UI 종료");
+      setIsChief(false);
+    }
+  }, [chiefPlayerId, remote, isGameState]);
 
   return (
     <>
       <li
         className={`${S.remoteParticipantOverlay} ${activePlayerId === remote.participant.identity ? S.active : ""}`}
-        onClick={isRemoteOverlay && !diedPlayer ? (e) => clickHandler(e, remote.participant.identity) : undefined}
+        onClick={isRemoteOverlay && !isDiedPlayer ? (e) => clickHandler(e, remote.participant.identity) : undefined}
       >
         <ParticipantTile
           // disableSpeakingIndicator={true}
-          className={`${S.remoteCam} ${isRemoteOverlay && !diedPlayer ? "cursor-pointer" : ""}`}
+          className={`${S.remoteCam} ${isRemoteOverlay && !isDiedPlayer ? "cursor-pointer" : ""}`}
         />
-        <div className={S.remoteChief}>
-          {isGameState === "gameReady" && <Image src={ChiefImage} alt={remote.participant.identity} />}
-        </div>
+        <div className={S.remoteChief}>{isChief && <Image src={ChiefImage} alt={remote.participant.identity} />}</div>
         {isGameState === "gameStart" && <p className={S.remotePlayerNumber}>{playerNumber}번</p>}
-        {!diedPlayer ? (
+        {!isDiedPlayer ? (
           <div className={`${S.remoteOverlay} ${remoteReadyStates[remote.participant.identity] ? S.active : ""}`}>
             <Image src={imageState || CamCheck} alt={remote.participant.identity} />
           </div>
